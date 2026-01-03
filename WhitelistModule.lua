560fb15c87704d405216e6b4da6c384dc03a8a85
1Markiiz <markiiz.sub@gmail.com>
2026-01-03 17:48:45 +0200

    Update WhitelistModule.lua

--- WhitelistModule.lua
+++ WhitelistModule.lua
@@ -1,134 +1,158 @@
--- WhitelistModule
 local WhitelistModule = {}
 
--- Rank structure:
--- RankName = {
---     Team = "TeamName",
---     Users = { UserIds }
--- }
+--// Rank structure:
+-- Priority = Higher number = higher rank
 
 WhitelistModule.Ranks = {
 
-    -- Ownership
-    ["Chairman"] = {
-        Team = "Ownership",
-        Users = {
-            3569563947,
-        }
-    },
+	--// OWNERSHIP
+	["Chairman"] = {
+		Priority = 100,
+		Team = "Ownership",
+		Users = {
+			3569563947,
+		}
+	},
 
-    ["Vice Chairman"] = {
-        Team = "Ownership",
-        Users = {
-            44444444,
-            1241683400,
-        }
-    },
+	["Vice Chairman"] = {
+		Priority = 90,
+		Team = "Ownership",
+		Users = {
+			1241683400,
+		}
+	},
 
-    -- Development / Automation
-    ["Developer"] = {
-        Team = "Ownership",
-        Users = {}
-    },
+	["Developer"] = {
+		Priority = 80,
+		Team = "Ownership",
+		Users = {}
+	},
 
+	--// CORPORATE
+	["Department Director"] = {
+		Priority = 70,
+		Team = "Corporate",
+		Users = {}
+	},
 
-    -- Corporate
-    ["Department Director"] = {
-        Team = "Corporate",
-        Users = {}
-    },
+	["Assistant Director"] = {
+		Priority = 65,
+		Team = "Corporate",
+		Users = {}
+	},
 
-    ["Assistant Director"] = {
-        Team = "Corporate",
-        Users = {}
-    },
+	["Senior Corporate"] = {
+		Priority = 60,
+		Team = "Corporate",
+		Users = {}
+	},
 
-    ["Senior Corporate"] = {
-        Team = "Corporate",
-        Users = {}
-    },
+	["Junior Corporate"] = {
+		Priority = 55,
+		Team = "Corporate",
+		Users = {}
+	},
 
-    ["Junior Corporate"] = {
-        Team = "Corporate",
-        Users = {}
-    },
+	["Corporate Intern"] = {
+		Priority = 50,
+		Team = "Corporate",
+		Users = {}
+	},
 
-    ["Corporate Intern"] = {
-        Team = "Corporate",
-        Users = {}
-    },
+	["Public Relations"] = {
+		Priority = 45,
+		Team = "Corporate",
+		Users = {}
+	},
 
-    ["Public Relations"] = {
-        Team = "Corporate",
-        Users = {}
-    },
+	--// MANAGEMENT
+	["Branch Manager"] = {
+		Priority = 40,
+		Team = "Management",
+		Users = {
+			1,
+		}
+	},
 
-    -- Management
-    ["Branch Manager"] = {
-        Team = "Management",
-        Users = {
-            1,
-        }
-    },
+	["Branch Supervisor"] = {
+		Priority = 35,
+		Team = "Management",
+		Users = {}
+	},
 
-    ["Branch Supervisor"] = {
-        Team = "Management",
-        Users = {}
-    },
+	["Assistant Supervisor"] = {
+		Priority = 30,
+		Team = "Management",
+		Users = {}
+	},
 
-    ["Assistant Supervisor"] = {
-        Team = "Management",
-        Users = {}
-    },
+	--// STAFF
+	["Kitchen Leader"] = {
+		Priority = 25,
+		Team = "Team Member",
+		Users = {}
+	},
 
-    -- Kitchen / Staff
-    ["Kitchen Leader"] = {
-        Team = "Team Member",
-        Users = {}
-    },
+	["Senior Team Member"] = {
+		Priority = 20,
+		Team = "Team Member",
+		Users = {}
+	},
 
-        ["Senior Team Member"] = {
-        Team = "Team Member",
-        Users = {}
-    },
+	["Team Member"] = {
+		Priority = 15,
+		Team = "Team Member",
+		Users = {}
+	},
 
-    ["Team Member"] = {
-        Team = "Team Member",
-        Users = {}
-    },
+	["Junior Team Member"] = {
+		Priority = 10,
+		Team = "Team Member",
+		Users = {
+			33,
+			23113,
+		}
+	},
 
-    ["Junior Team Member"] = {
-        Team = "Team Member",
-        Users = {
-            0033,
-            0023113,
-        }
-    },
+	--// CUSTOMERS
+	["Trainee"] = {
+		Priority = 5,
+		Team = "Costumer",
+		Users = {}
+	},
 
-        ["Trainee"] = {
-        Team = "Costumer",
-        Users = {}
-    },
-            ["Noted Customer"] = {
-        Team = "Costumer",
-        Users = {}
-    },
-                ["Customer"] = {
-        Team = "Costumer",
-        Users = {}
-    },
+	["Noted Customer"] = {
+		Priority = 3,
+		Team = "Costumer",
+		Users = {}
+	},
+
+	["Customer"] = {
+		Priority = 1,
+		Team = "Costumer",
+		Users = {}
+	},
 }
 
--- Returns rank name + team name
+--// Returns highest priority rank
 function WhitelistModule:GetRank(player)
-    for rankName, data in pairs(self.Ranks) do
-        for _, userId in ipairs(data.Users) do
-            if player.UserId == userId then
-                return rankName, data.Team
-            end
-        end
-    end
-    return nil, nil
+	local bestRank = nil
+	local bestPriority = -math.huge
+
+	for rankName, data in pairs(self.Ranks) do
+		for _, userId in ipairs(data.Users) do
+			if player.UserId == userId and data.Priority > bestPriority then
+				bestPriority = data.Priority
+				bestRank = rankName
+			end
+		end
+	end
+
+	if bestRank then
+		return bestRank, self.Ranks[bestRank].Team
+	end
+
+	return nil, nil
 end
 
 return WhitelistModule
